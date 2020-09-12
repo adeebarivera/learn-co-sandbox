fa4a600a6d19140b3e6e486f7217ca3d7b978d96
adeeba reaz <adeebarivera@outlook.com>
2020-09-12 00:32:00 +0000

    adding code

--- makeup_cli/lib/make_up/api.rb
+++ makeup_cli/lib/make_up/api.rb
@@ -1,3 +1,10 @@
-class Api 
-  puts "api class loaded"
+class Api
+  
+  BASE_URL = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline'
+
+  def self.get_makeup
+    res = RestClient.get(BASE_URL)
+    binding.pry
+  end
 end
+
